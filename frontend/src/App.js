3f691308f8c0ad80dd09a397fd89fbc994dcfbb1
unknown <achyuth.paddy@gmail.com>
2026-02-20 10:37:59 +0530

    gatekeeping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,67 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.js";
 import Footer from "./Components/Footer/Footer.js";
 import Home from "./Pages/Home/Home.js";
 import Register from "./Pages/Register/Register.js";
 import Login from "./Pages/Login/Login.js";
 import Transactions from "./Pages/Transactions/Transactions.js";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute.js";
+
 // import Dashboard from "./Pages/Dashboard/Dashboard.js";
 
-
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <Router>
       <Navbar />
       <Routes>
-       <Route path="/" element={<Home />} /> {/* Default route */} 
-       <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} /> {/* Home now at /home */}
-        <Route path="/transactions" element={<Transactions />} />
-        {/* //<Route path="/dashboard" element={<Dashboard />} /> */}
+
+        {/* Default Route Logic */}
+        <Route
+          path="/"
+          element={
+            token ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
+          }
+        />
+
+        {/* Public Routes */}
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={
+            token ? <Navigate to="/home" replace /> : <Login />
+          }
+        />
+
+        {/* Protected Routes */}
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/transactions"
+          element={
+            <PrivateRoute>
+              <Transactions />
+            </PrivateRoute>
+          }
+        />
+
+        {/* <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        /> */}
+
       </Routes>
       <Footer />
     </Router>
